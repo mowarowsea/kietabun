4509b28a957cedcd382f630113dc75c4be26165d
mowa <morerowsea@gmail.com>
2025-07-17 01:09:50 +0900

    環境変数の直し忘れ修正

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
-DATABASE_API_KEY = os.environ.get("DATABASE_API_KEY")
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_API_KEY = os.environ.get("SUPABASE_API_KEY")
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_ANON_KEY = os.environ.get("SUPABASE_ANON_KEY")
