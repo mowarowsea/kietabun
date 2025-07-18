0a5fa2f871b694f051836dd55a7687bec194b7ec
mowa <morerowsea@gmail.com>
2025-07-19 00:39:09 +0900

    仕切り直しで最小構成にしてみる。

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-SUPABASE_URL = os.environ.get("SUPABASE_URL")
-SUPABASE_API_KEY = os.environ.get("SUPABASE_API_KEY")
-SUPABASE_ANON_KEY = os.environ.get("SUPABASE_ANON_KEY")
